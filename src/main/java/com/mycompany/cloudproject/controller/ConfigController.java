9911c35ec4a7102b28fd0c45f0a8cfc55655152a
Manali Raut <raut.ma@northeastern.edu>
2024-09-25 14:53:19 -0400

    Adding files for HealthCheckup API
    Implemention of GET request to check db health

---
+++ src/main/java/com/mycompany/cloudproject/controller/ConfigController.java
@@ -0,0 +1,63 @@
+package com.mycompany.cloudproject.controller;
+
+import com.mycompany.cloudproject.dao.DBConfiguration;
+import com.mycompany.cloudproject.service.ConfigService;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ConfigController {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigController.class.getName());
+
+    @Autowired
+    ConfigService service;
+
+    @GetMapping("/healthz")
+    @ResponseBody
+    public ResponseEntity<String>  getConfig(@RequestBody(required = false)String payload, HttpServletResponse response){
+
+        setResponseHeaders(response);
+        if (payload != null && !payload.isEmpty()) {
+            logger.info("Payload is not supported. Bad Request");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+
+        if (service.getConfig()){
+            logger.info("Getting success response");
+            return ResponseEntity.ok().build();
+        }
+        logger.info("Service is unavailable now. Please try again later.");
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).build();
+    }
+
+    public void setResponseHeaders(HttpServletResponse response) {
+        logger.info("setting response headers");
+        response.setHeader("cache-control", "no-cache, no-store, must-revalidate");
+        response.setHeader("Pragma", "no-cache");
+        response.setHeader("X-Content-Type-Options", "nosniff");
+        logger.info("Finished setting response headers");
+    }
+
+    @RequestMapping(path = "/healthz", method = {RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE, RequestMethod.HEAD, RequestMethod.OPTIONS, RequestMethod.TRACE})
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
+    public void unSupportedMethods(HttpServletResponse response) {
+        logger.error("Unsupported HTTP method");
+        setResponseHeaders(response);
+    }
+
+    @RequestMapping(path = "/**", method = {RequestMethod.GET,RequestMethod.POST, RequestMethod.PUT, RequestMethod.PATCH, RequestMethod.DELETE, RequestMethod.HEAD, RequestMethod.OPTIONS, RequestMethod.TRACE})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public void wrongURL(HttpServletResponse response) {
+        logger.error("wrong URL");
+        setResponseHeaders(response);
+    }
+
+
+}
