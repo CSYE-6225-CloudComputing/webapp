3d6abaff89c0a02d6eade9edb7efa691604d53a9
Manali Raut <raut.ma@northeastern.edu>
2024-10-14 20:31:00 -0400

    updating new workflows

--- packer/setup.sh
+++ packer/setup.sh
@@ -36,7 +36,7 @@ DATABASE_NAME='cloud'  # Change this to your desired database name
 DB_USER='raut'         # Change this to your desired database username
 
 # Login as root and create user and database
-sudo mysql -u root -p root <<EOF
+sudo mysql -u root -proot <<EOF
 CREATE DATABASE $DATABASE_NAME;
 CREATE USER '$DB_USER'@'localhost' IDENTIFIED BY '$NEW_PASSWORD';
 GRANT ALL PRIVILEGES ON $DATABASE_NAME.* TO '$DB_USER'@'localhost';
@@ -57,7 +57,7 @@ sudo useradd -g csye6225 --shell /usr/sbin/nologin csye6225
 # Change ownership of the necessary directories/files
 echo 'Changing ownership of application files to csye6225 user and group'
 sudo mkdir -p /opt/cloudproject
-mv /tmp/target/cloudproject-0.0.1-SNAPSHOT.war /opt/cloudproject
+mv /tmp/cloudproject-0.0.1-SNAPSHOT.war /opt/cloudproject
 sudo chown -R csye6225:csye6225 /opt/cloudproject
 
 # Clean up
